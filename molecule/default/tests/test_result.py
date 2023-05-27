bf4279b561d848a971264c3c99edc64a7263a209
Pim van Helvoirt <pim.van.helvoirt@home.nl>
2023-05-27 13:24:34 +0200

    test: added additional, cleaned up function comments (#4)

--- molecule/default/tests/test_result.py
+++ molecule/default/tests/test_result.py
@@ -8,19 +8,34 @@ def test_os_release(host):
 
 
 def test_ssh_running(host):
-    """test that the fail2ban service is running"""
+    """test that the ssh service is running"""
 
     assert host.service("ssh").is_running
 
 
 def test_ssh_certs_created(host):
-    """test that the fail2ban jail.local file is created"""
+    """test that the ssh certificates are created"""
 
     for crypto in ["rsa", "ecdsa", "ed25519"]:
         assert host.file(f"/etc/ssh/ssh_host_{crypto}_key-cert.pub")
 
 
-def test_validity_hostkey(host):
+def test_auth_principals_created(host):
+    """test that the auth principals are added correctly"""
+
+    with host.sudo():
+        assert host.file("/etc/ssh/auth_principals/ansible")
+        assert host.file("/etc/ssh/auth_principals/ansible").contains("testing")
+
+
+def test_validate_hostkeys_script_exists(host):
+    """test that the validate_hostkeys script is present"""
+
+    with host.sudo():
+        assert host.file("/usr/local/bin/validate_hostkeys")
+
+
+def test_validate_hostkey_timer_runner(host):
     """test that the validate_hostkeys timer is running"""
 
     with host.sudo():
